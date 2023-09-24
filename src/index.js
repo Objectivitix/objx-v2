ca9d50198c4a45b1c304c230fcd776b7d006ed9d
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2023-09-24 16:15:50 -0400

    Add cool skewed background

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const root = document.documentElement;
+const header = document.querySelector("header");
+
+document.addEventListener("scroll", () => {
+  const viewportHeight = Math.max(root.clientHeight, window.innerHeight ?? 0);
+  const scrolled = window.scrollY > viewportHeight * 0.6;
+
+  header.classList.toggle("header--bg", scrolled);
+});
